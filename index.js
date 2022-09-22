6b0b7dd8393c94bba9f3f4e4a8b8aa0c49862536
Nelson Choque <nelsonchoque03@gmail.com>
2022-09-21 22:32:26 -0500

    agregando json productos

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
 
+const $templateProduct= document.getElementById("template-product").content;
+let $fragment= document.createDocumentFragment();
+const $products= document.getElementById("products");
+
 (()=>{
 
     console.log("hola")
@@ -10,18 +14,68 @@
 
 
 
-function showProducts(){
+async function getDataProducts(){
 
-
-    fetch('/productos.json')
+    const dataProducts= await fetch('/productos.json')
     .then((data)=>{
         
         return data.json();
 
     })
-    .then((data)=>{
-        console.log(data.products[0])
-    })
+
+
+    return dataProducts;
+}
+
+
+async function showProducts(){
+
+
+    const {products} = await getDataProducts();
+
+    let {templateImage,templateBrand,templateName,templatePrice} = getTemplateElementsProducts();
+
+    let array = []
+    
+    
+    products.forEach(product => {
+
+
+
+        templateImage.setAttribute("src",product.image)
+        templateBrand.textContent = product.brand
+        templateName.textContent = product.name
+        templatePrice.textContent = product.price
+
+        let $clone = document.importNode($templateProduct,true);
+
+        $fragment.appendChild($clone)
+
+        
+    });
+
+    console.log($fragment)
+    console.log($products)
+    $products.appendChild($fragment)
+
+
+
+}
+
+function getTemplateElementsProducts(){
+
+    let templateImage = $templateProduct.querySelector(".products__item__image")
+    let templateBrand = $templateProduct.querySelector(".products__item__brand")
+    let templateName= $templateProduct.querySelector(".products__item__name")
+    let templatePrice = $templateProduct.querySelector(".products__item__price")
+
+    return {
+        templateImage,
+        templateBrand,
+        templateName,
+        templatePrice
+    }
+
 
 
 }
